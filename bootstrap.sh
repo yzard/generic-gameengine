9b8325b58f5a7a52f427df10b7d9ba455ed714c6
Zhuo Yin <zyin@zyin-router.(none)>
2011-03-19 05:01:08 -0400

    autoconf tuning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 
 aclocal && autoreconf -f -i -Wall,no-obsolete 
-[ -d ./autom4te.cache ] && rm -rf ./autom4te.cache
 
 automake --add-missing
+
+# remove some junk
+rm -rf ./autom4te.cache
+rm ./config.h.in~
+
+
