28d7aa563df096d9d49dcbbdf6c2ceb0ebedb2f3
Zhuo Yin <zhuoyin@gmail.com>
2011-03-21 17:06:39 -0400

    lock free single feeder single consumer queue implemented

---
+++ libinfra/infra/Queue.h
@@ -0,0 +1,123 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include "Global.h"
+#include <cstring>
+#include <stdint.h>
+
+/* This is a lock-free FIFO queue template implementation.
+ * the queue size has to be 2^n
+ * the Type can be char, int, and any fixed size of type
+ * if for class, the POINTER of class is highly recommanded.
+ * For example:
+ * 	Queue<int> 	int_queue;
+ *	Queue<Task*>	task_queue; // Task is a class here.
+ */
+
+#define DEFAULT_QUEUE_SIZE 1024
+
+template <class Type>
+class Queue {
+private:
+	Type*		buffer;		// Ring buffer
+	uint32_t	size;		// size of the buffer
+	uint32_t	in;		// index for write
+	uint32_t	out;		// index for read
+	uint32_t	typeSize;	// sizeof(Type)
+
+public:
+	// Initialize the FIFO with size
+	// the size should be 2^n
+	Queue(uint32_t init_size = DEFAULT_QUEUE_SIZE) {
+		// check if the size is 2^n
+		if (init_size & (init_size - 1)) {
+			// the size is not 2^n, set it
+			// to default queue size
+			init_size = DEFAULT_QUEUE_SIZE;
+		};
+
+		size 		= init_size;
+
+		// initialize the buffer with type T
+		buffer 		= new Type[size];
+
+		// if in == out, means empty
+		in		= 0;
+		out		= 0;
+
+		// sizeof(Type), will be used for memcpy
+		typeSize = sizeof(Type);
+	}
+	~Queue() {
+		delete[] buffer;
+	}
+
+	// the method for thread to put stuff in the queue
+	// can be number of stuff
+	uint32_t put(Type* elements, uint32_t len) {
+		uint32_t endLen;
+
+		// size - (in - out) means the free space of the queue
+		// if the number is larger than the free space of
+		// FIFO, then use the free space instead
+		len = min(len, size - (in - out));
+
+		// Ensure we sample the out index before we start putting
+		// bytes into buffer
+		mb();
+
+		// first put the data starting from in to buffer end
+		endLen = min(len, size - (in & (size-1)));
+		memcpy(buffer + (in & (size-1)), elements, endLen*typeSize);
+
+		// then put the rest at the beggining of the buffer
+		memcpy(buffer, elements + endLen, (len-endLen)*typeSize);
+		
+		// Ensure we add the bytes to the buffer before we
+		// update the in index
+		wmb();
+
+		in += len;
+
+		// return the length of elements has been written.
+		return len;			
+	};
+
+	// the method for another thread to get stuff off the queue
+	uint32_t get(Type* elements, uint32_t len) {
+		uint32_t endLen;
+
+		len = min(len, in - out);
+
+		// ensure sample the in index before we start removing
+		// bytes from the buffer
+		rmb();
+
+		// first get the data from out to the end of the buffer
+		endLen = min(len, size - (out & (size - 1)));
+		memcpy(elements, buffer + (out & (size - 1)), endLen*typeSize);
+
+		// then get the rest data from the begining of the buffer
+		memcpy(elements + endLen, buffer, (len - endLen)*typeSize);
+
+		// ensure we remove the bytes from the buffer before we
+		// update the out index
+		mb();
+
+		out += len;
+
+		// return the length of elements has been read.
+		return len;
+	}
+
+	uint32_t length() {
+		return size;
+	}
+
+	uint32_t count() {
+		return in - out;
+	}
+
+};
+
+#endif /*  __QUEUE_H__ */
---
+++ test/function/queue_test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <../../libinfra/infra/Queue.h>
+#include <pthread.h>
+#include <unistd.h>
+
+Queue<int> queue;
+
+void *feeder(void *id) {
+	const int all = 2000;
+	const int step = 5;
+
+	int array[all];
+	for (int i=0; i< all; i++)
+		array[i] = i;
+
+	int pushed 	= 0;
+	int counter 	= 0;
+
+	while (counter < all) {
+		if (counter + step >= all)
+			pushed = queue.put(&array[counter], all - counter);
+		else
+			pushed = queue.put(&array[counter], step);
+		counter += pushed;
+	}
+
+	pthread_exit(NULL);
+}
+
+void *consumer(void *id) {
+	const int step = 5;
+	int array[step];
+	int received;
+	while (true) {
+		received = queue.get(array, step);
+		if (received > 0) {
+			std::cout << "Received:";
+			for (int i = 0; i < received; i++)
+				std::cout << array[i] << " ";
+			std::cout << std::endl;
+
+		}
+	}
+}
+
+int main() {
+	pthread_t threads[2];
+
+	pthread_create(&threads[0], NULL, feeder, (void*)0);
+	pthread_create(&threads[1], NULL, consumer, (void*)1);
+
+	pthread_exit(NULL);
+}
