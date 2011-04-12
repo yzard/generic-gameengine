e66205e07e6d84f98cfe5970e7c0b7613923ab43
Zhuo Yin <zhuoyin@gmail.com>
2011-04-12 13:34:45 -0400

    test TRACE

--- test/function/threadpool_test.cpp
+++ test/function/threadpool_test.cpp
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include <infra/ThreadPool.h>
 #include <infra/ExampleTask.h>
+#include <infra/Debugging.h>
 
 void signalHandler(int sig) {
 	ThreadPool* pool = ThreadPool::Instance();
@@ -17,6 +18,9 @@ int main() {
 	//ExampleTask** tasks = new ExampleTask*[10];
 
 	//pool->add(tasks, 10);
+
+	int i = 1;
+	TRACE(i);
 
 	// Join all
 	pool->JoinAll();
